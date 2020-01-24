788237406cbef69129803f715b149f2d1697fcb9
John Speck <speckworks@gmail.com>
2020-01-24 14:41:18 -0500

    added semantic cards

--- src/dinoCard.js
+++ src/dinoCard.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
+import { Card, Image } from 'semantic-ui-react'
 
 export default class dinoCard extends Component {
     render() {
         let card = this.props.dinocard
-        {console.log("dinocard", this.props.dinocard)}
+        // {console.log("dinocard", this.props.dinocard)}
         return (
-            <div    className="app" 
-                    style={{
+            <Card>
+            <Image src={card.img}/>
+            <Card.Content>
+              <Card.Header>{card.name}</Card.Header>
+              <Card.Description style={{
                     background:card.color}}>
-            {card.name}
-            <button onClick={()=>this.props.deleteDino(card.id)} 
+              </Card.Description>
+            </Card.Content>
+            <Card.Content>
+              <button onClick={()=>this.props.deleteDino(card.id)} 
                     className="deleteButton">Delete</button>
-            <img    src={`${card.img}`}
-                    alt="dinosaur" 
-                    className="card"></img> 
-            
-            </div>
+            </Card.Content>
+          </Card>
         )
     }
 }
+
+
+// <div    className="app" 
+//                     style={{
+//                     background:card.color}}>
+//             {card.name}
+//             <button onClick={()=>this.props.deleteDino(card.id)} 
+//                     className="deleteButton">Delete</button>
+//             <img    src={`${card.img}`}
+//                     alt="dinosaur" 
+//                     className="card"></img> 
+            
+//             </div>
