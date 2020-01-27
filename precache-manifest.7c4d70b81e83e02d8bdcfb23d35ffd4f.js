d56263231d043b607e040663b001e3358a6144d1
John Speck <speckworks@gmail.com>
2020-01-27 11:40:25 -0500

    Updates

---
+++ precache-manifest.7c4d70b81e83e02d8bdcfb23d35ffd4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d717f51cb2a09debc0959099806686",
+    "url": "/dinocards_frontend/index.html"
+  },
+  {
+    "revision": "24315f4a104b99d789ea",
+    "url": "/dinocards_frontend/static/css/main.afa29195.chunk.css"
+  },
+  {
+    "revision": "d268630ca4831824cc62",
+    "url": "/dinocards_frontend/static/js/2.8977541e.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/dinocards_frontend/static/js/2.8977541e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24315f4a104b99d789ea",
+    "url": "/dinocards_frontend/static/js/main.e51c207d.chunk.js"
+  },
+  {
+    "revision": "56c772af720b154b84eb",
+    "url": "/dinocards_frontend/static/js/runtime-main.7a614b08.js"
+  }
+]);
