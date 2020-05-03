5d2c1a07afb6ea64b989b6cf9d5d3353b3ab533a
John Speck <speckworks@gmail.com>
2020-05-02 21:43:09 -0400

    Updates

---
+++ precache-manifest.7830bf6d7e3eecd99a34b22bf6343e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91413b96741544b3bb115ff18eea2f45",
+    "url": "/dinocards_frontend/index.html"
+  },
+  {
+    "revision": "2290b72a1ad9177acc74",
+    "url": "/dinocards_frontend/static/css/main.2d63ac08.chunk.css"
+  },
+  {
+    "revision": "5f6c47f5c6b2ba2e5a6f",
+    "url": "/dinocards_frontend/static/js/2.5b3f19a5.chunk.js"
+  },
+  {
+    "revision": "6ad3abf20cc4e53b1b42f6c2844608bf",
+    "url": "/dinocards_frontend/static/js/2.5b3f19a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2290b72a1ad9177acc74",
+    "url": "/dinocards_frontend/static/js/main.f9015722.chunk.js"
+  },
+  {
+    "revision": "794204de400c2a97c5b7",
+    "url": "/dinocards_frontend/static/js/runtime-main.0958e881.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dinocards_frontend/precache-manifest.52b60b2bc79acdb3d091b8ff459520f0.js"
+  "/dinocards_frontend/precache-manifest.7830bf6d7e3eecd99a34b22bf6343e33.js"
 );
 
 self.addEventListener('message', (event) => {
